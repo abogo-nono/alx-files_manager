b36fbd0a9b24eb08ea52ba6cc9a967ef5e7e76fb
Abogo <abogonono1@gmail.com>
2025-01-26 02:13:17 +0100

    endpoints added 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const express = require('express');
+const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-
-const router = express.Router();
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -13,5 +13,10 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
+router.get('/users/:id/files', FilesController.getIndex);
+router.post('/users/:id/files', FilesController.postUpload);
+router.get('/users/:id/files/:fileId', FilesController.getShow);
+router.put('/users/:id/files/:fileId/publish', FilesController.putPublish);
+router.put('/users/:id/files/:fileId/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
